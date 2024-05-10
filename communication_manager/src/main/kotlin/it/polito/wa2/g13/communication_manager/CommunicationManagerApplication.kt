931a6ced7cb67b29d6f22965ba5e4354fe5a5934
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-05-10 14:21:08 +0200

    init

---
+++ communication_manager/src/main/kotlin/it/polito/wa2/g13/communication_manager/CommunicationManagerApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g13.communication_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunicationManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunicationManagerApplication>(*args)
+}
---
+++ communication_manager/src/test/kotlin/it/polito/wa2/g13/communication_manager/CommunicationManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.g13.communication_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunicationManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
