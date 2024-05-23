0fea91fcd7695f1068876656ef91030a73c65efd
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-05-23 20:58:58 +0200

    fix: removed comments and fixed env fallback when empty

--- communication_manager/src/main/kotlin/it/polito/wa2/g13/communication_manager/routes/GmailRoute.kt
+++ communication_manager/src/main/kotlin/it/polito/wa2/g13/communication_manager/routes/GmailRoute.kt
@@ -5,43 +5,25 @@ import it.polito.wa2.g13.communication_manager.dtos.CreateMessageDTO
 import it.polito.wa2.g13.communication_manager.dtos.Priority
 import org.apache.camel.EndpointInject
 import org.apache.camel.Exchange
+import org.apache.camel.LoggingLevel
+import org.apache.camel.RuntimeCamelException
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.component.google.mail.GoogleMailEndpoint
-import org.slf4j.LoggerFactory
+import org.apache.hc.client5.http.HttpHostConnectException
 import org.springframework.stereotype.Component
 
 @Component
 class GmailRoute(
     private val crmConfig: CrmConfigProperties
 ) : RouteBuilder() {
-    companion object {
-        private val logger = LoggerFactory.getLogger(GmailRoute::class.java)
-    }
-
     @EndpointInject("google-mail:messages/get")
     lateinit var ep: GoogleMailEndpoint
-
-//    @EndpointInject("google-mail:attachments/get")
-//    lateinit var attachmentsEp: GoogleMailEndpoint
 
     override fun configure() {
         from("google-mail-stream:0?markAsRead=true&scopes=https://mail.google.com/")
             .process {
                 val id = it.getIn().getHeader("CamelGoogleMailId").toString()
                 val message = ep.client.users().messages().get("me", id).execute()
-
-//                val attachmentId = message
-//                    .payload
-//                    .parts
-//                    .find { part -> part.body.attachmentId != null }
-//                    ?.body
-//                    ?.attachmentId
-//
-//                logger.info(it.getIn().headers.toString())
-//                logger.info(attachmentId)
-//                val attachment =
-//                    attachmentsEp.client.users().messages().attachments().get("me", message.id, attachmentId).execute()
-//                logger.info(attachment.toString())
 
                 val subject = message
                     .payload
@@ -67,11 +49,10 @@ class GmailRoute(
                     body = message.snippet,
                 )
             }
-            .log("Sending email from \${body.sender} to CRM service.")
+            .log(LoggingLevel.INFO, "Sending email from \${body.sender} to CRM service.")
             .marshal().json()
             .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http.HttpMethods.POST))
             .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
             .to("${crmConfig.url}:${crmConfig.port}/API/messages")
-//            .to("bean:emailRepository?method=save")
     }
 }
