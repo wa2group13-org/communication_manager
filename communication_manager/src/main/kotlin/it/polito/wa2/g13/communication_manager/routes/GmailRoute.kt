33f9a72faebf1e672408193455039d54dec4cf83
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-05-30 19:34:39 +0200

    feat: split google-mail-stream route into multiple routes for readability

--- communication_manager/src/main/kotlin/it/polito/wa2/g13/communication_manager/routes/GmailRoute.kt
+++ communication_manager/src/main/kotlin/it/polito/wa2/g13/communication_manager/routes/GmailRoute.kt
@@ -1,5 +1,6 @@
 package it.polito.wa2.g13.communication_manager.routes
 
+import com.google.api.services.gmail.model.Message
 import com.google.api.services.gmail.model.MessagePart
 import com.google.api.services.gmail.model.MessagePartBody
 import it.polito.wa2.g13.communication_manager.configurations.CrmConfigProperties
@@ -12,6 +13,7 @@ import org.apache.camel.Exchange
 import org.apache.camel.LoggingLevel
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.component.google.mail.GoogleMailEndpoint
+import org.apache.camel.component.google.mail.stream.GoogleMailStreamConstants
 import org.apache.hc.client5.http.HttpHostConnectException
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder
 import org.apache.hc.client5.http.entity.mime.StringBody
@@ -28,14 +30,26 @@ class GmailRoute(
     private val crmConfig: CrmConfigProperties,
     private val documentStoreConfig: DocumentStoreConfigProperties,
 ) : RouteBuilder() {
+    companion object {
+        const val MAIN_ROUTE_ID = "receiveEmailFromGmail"
+    }
+
+    /** Direct routes names */
+    object Direct {
+        const val GET_ATTACHMENTS = "direct:getAttachments"
+        const val GET_GMAIL_MESSAGE = "direct:getGmailMessage"
+        const val SEND_MESSAGE_TO_CRM = "direct:sendMessageToCrm"
+        const val SEND_ATTACHMENT_TO_DOCUMENT_STORE = "direct:sendAttachmentToDocumentStore"
+    }
+
     @EndpointInject("google-mail:messages/get")
-    lateinit var ep: GoogleMailEndpoint
+    lateinit var messagesEp: GoogleMailEndpoint
 
     @EndpointInject("google-mail:attachments/get")
     lateinit var attachmentsEp: GoogleMailEndpoint
 
     override fun configure() {
-        onException(HttpHostConnectException::class.java)
+        onException(Exception::class.java)
             .maximumRedeliveries(0)
             .log(
                 LoggingLevel.ERROR,
@@ -44,9 +58,54 @@ class GmailRoute(
             .markRollbackOnly()
 
         from("google-mail-stream:0?markAsRead=true&scopes=https://mail.google.com/")
+            .routeId(MAIN_ROUTE_ID)
+            .log("Started processing mail with id: \${headers.${GoogleMailStreamConstants.MAIL_ID}}")
+            // Set the current message read from the stream
+            .to(Direct.GET_GMAIL_MESSAGE)
+            .to(Direct.GET_ATTACHMENTS)
+            .to(Direct.SEND_MESSAGE_TO_CRM)
+            // Send all the attachments to the document_store service
+            .split(variable("attachments"))
+            .to(Direct.SEND_ATTACHMENT_TO_DOCUMENT_STORE)
+            .end()
+            .log("Processing email from \"\${variable.sender}\" terminated.")
+
+        // Get the email from gmail
+        from(Direct.GET_GMAIL_MESSAGE)
             .process {
-                val id = it.getIn().getHeader("CamelGoogleMailId").toString()
-                val message = ep.client.users().messages().get("me", id).execute()
+                it.getIn().body = messagesEp.client.users().messages()
+                    .get("me", it.getIn().getHeader(GoogleMailStreamConstants.MAIL_ID).toString()).execute()
+            }
+
+        // Get attachments from gmail
+        from(Direct.GET_ATTACHMENTS)
+            // Get the attachments of the current message and put them in a variable
+            .process {
+                val message = it.getIn().getBody(Message::class.java)
+
+                val parts = message
+                    .payload
+                    ?.parts
+                    ?.filter { part -> part?.body?.attachmentId != null }
+                    ?: listOf()
+
+                val attachments = parts
+                    .map { part ->
+                        Attachment(
+                            part = part,
+                            attachment = attachmentsEp.client.users().messages().attachments()
+                                .get("me", message.id, part.body.attachmentId)
+                                .execute()
+                        )
+                    }
+
+                it.setVariable("attachments", attachments)
+            }
+
+        // Send message to crm
+        from(Direct.SEND_MESSAGE_TO_CRM)
+            .process {
+                val message = it.getIn().getBody(Message::class.java)
 
                 val subject = message
                     .payload
@@ -72,39 +131,20 @@ class GmailRoute(
                     body = message.snippet,
                 )
 
-                val parts = message
-                    .payload
-                    ?.parts
-                    ?.filter { part -> part?.body?.attachmentId != null }
-                    ?: listOf()
-
-                val attachments = parts
-                    .map { part ->
-                        Attachment(
-                            part = part,
-                            attachment = attachmentsEp.client.users().messages().attachments()
-                                .get("me", message.id, part.body.attachmentId)
-                                .execute()
-                        )
-
-                    }
-
                 it.setVariable("sender", from)
-                it.setVariable("attachments", attachments)
             }
             .log(LoggingLevel.INFO, "Sending email from \"\${body.sender}\" to CRM service.")
             .marshal().json()
             .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http.HttpMethods.POST))
             .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-            .to("${crmConfig.url}:${crmConfig.port}/API/messages")
+            .to("${crmConfig.url}:${crmConfig.port}/API/messages?throwExceptionOnFailure=false")
             .unmarshal().json(CrmSendMessageResponse::class.java)
             // Save the `id` of the last created message into the variables
             .process { it.setVariable("messageId", it.getIn().getBody(CrmSendMessageResponse::class.java).id) }
-            // Send all the attachments to the document_store service
-            .split(variable("attachments"))
-            .to("direct:handleAttachments")
+            .setVariable("messageId", simple("\${body.id}"))
 
-        from("direct:handleAttachments")
+        // Send attachment to document store
+        from(Direct.SEND_ATTACHMENT_TO_DOCUMENT_STORE)
             .log(
                 LoggingLevel.INFO,
                 "Sending attachments with name \"\${body.part.filename}\" of Message@\${variables.messageId} to document_store."
